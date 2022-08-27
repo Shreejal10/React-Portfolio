b436cf7917afd7bef6d81ee337b4eebd59d6bab6
Shreejal Dhungana <dshreejal@gmail.com>
2022-08-27 10:48:23 +0545

    projects component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 
 
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 }
