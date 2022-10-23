91c5b1c9dd5ca7d2d3aba195d843697ffd2355d8
Shreejal Dhungana <dshreejal@gmail.com>
2022-10-23 22:59:08 +0545

    button bg-change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,10 +11,10 @@ function Contact() {
                     <p className='text-gray-500 py-4'>Submit the form below or send me an email - dshreejal@gmail.com</p>
                 </div>
                 <input type="text" placeholder='Name' name="name"
-                    className='bg-gray-300 p-2' />
-                <input type="email" placeholder='Email' name="email" className='my-4 p-2 bg-gray-300' />
-                <textarea name="message" placeholder='Message' rows="10" className='bg-gray-300 p-2'></textarea>
-                <button className='text-white border-2 bg-[#a2d2ff] border-[#a2d2ff] hover:bg-[#56aeff] hover:border-[#56aeff] px-4 py-3 my-8 mx-auto flex items-center' >Let's Collaborate</button>
+                    className='bg-gray-300 p-2' required />
+                <input type="email" placeholder='Email' name="email" className='my-4 p-2 bg-gray-300' required />
+                <textarea name="message" placeholder='Message' rows="10" className='bg-gray-300 p-2' required></textarea>
+                <button className='text-white border-2 = bg-[#56aeff] border-[#56aeff] px-4 py-3 my-8 mx-auto flex items-center hover:scale-105' >Let's Collaborate</button>
             </form >
 
         </div >
