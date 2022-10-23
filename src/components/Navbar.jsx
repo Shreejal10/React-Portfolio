f624d96c011263c183df59d2f6dd5f598829d01c
Shreejal Dhungana <dshreejal@gmail.com>
2022-10-23 23:32:27 +0545

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
                 </NavLink>
             </div>
             {/* Menu */}
-            <ul className='hidden md:flex text-xl'>
+            <ul className='hidden lg:flex text-xl'>
                 <li className='hover:text-[#44BB88]'>
                     <NavLink to="/" end  >
                         Home
@@ -45,13 +45,13 @@ const Navbar = () => {
             </ul>
 
             {/* hamburger */}
-            <div onClick={handleClick} className='md:hidden z-10'>
+            <div onClick={handleClick} className='lg:hidden z-10'>
                 {!nav ? <FaBars /> : <FaTimes />}
             </div>
 
 
             {/* Mobile Menu */}
-            <ul className={!nav ? 'hidden' : 'absolute top-0 left-1/2 w-1/2 h-screen bg-[#BDE0FE] md:hidden flex flex-col  justify-center items-center'}>
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-1/2 w-1/2 md:left-2/3 md:w-1/3 h-screen bg-[#BDE0FE] lg:hidden flex flex-col  justify-center items-center'}>
                 <li className='py-6 text-3xl hover:text-[#44BB88]'>
                     <NavLink onClick={handleClick} to="/" end>
                         Home
@@ -77,33 +77,44 @@ const Navbar = () => {
                         Contact
                     </NavLink>
                 </li>
+                <div className='flex gap-6 pt-20'>
+                    <a href="https://www.facebook.com/Shreejal.Dhungana10" target="_blank" rel="noopener noreferrer">
+                        <FaFacebook size={25} />
+                    </a>
+                    <a href="https://www.instagram.com/shreejal_dhungana/" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram size={25} />
+                    </a>
+                    <a href="https://github.com/shreejal10" target="_blank" rel="noopener noreferrer">
+                        <FaGithub size={25} />
+                    </a>
+                </div>
             </ul>
 
             {/* Social icons */}
             <div className='hidden lg:flex fixed felx-col top-[30%] left-0'>
                 <ul>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-1px] duration-300 bg-blue-600'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href="https://www.facebook.com/Shreejal.Dhungana10" target="_blank" rel="noopener noreferrer">
+                    <li className='w-[140px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-1px] duration-300 bg-blue-600'>
+                        <a className='flex justify-between items-center w-full text-gray-300 gap-2' href="https://www.facebook.com/Shreejal.Dhungana10" target="_blank" rel="noopener noreferrer">
                             Facebook <FaFacebook size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-1px] duration-300 instagram'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href="https://www.instagram.com/shreejal_dhungana/" target="_blank" rel="noopener noreferrer">
+                    <li className='w-[140px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-1px] duration-300 instagram'>
+                        <a className='flex justify-between items-center w-full text-gray-300 gap-2' href="https://www.instagram.com/shreejal_dhungana/" target="_blank" rel="noopener noreferrer">
                             Instagram <FaInstagram size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-1px] duration-300 bg-[#171515]'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/shreejal10" target="_blank" rel="noopener noreferrer">
+                    <li className='w-[140px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-1px] duration-300 bg-[#171515]'>
+                        <a className='flex justify-between items-center w-full text-gray-300 gap-2' href="https://github.com/shreejal10" target="_blank" rel="noopener noreferrer">
                             Github <FaGithub size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-1px] duration-300 bg-[#0366C3]'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/shreejal-dhungana/" target="_blank" rel="noopener noreferrer">
+                    <li className='w-[140px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-1px] duration-300 bg-[#0366C3]'>
+                        <a className='flex justify-between items-center w-full text-gray-300 gap-2' href="https://www.linkedin.com/in/shreejal-dhungana/" target="_blank" rel="noopener noreferrer">
                             LinkedIn <FaLinkedin size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-1px] duration-300 bg-[#6fc2b0]'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href="/">
+                    <li className='w-[140px] h-[60px] flex justify-between items-center ml-[-90px] hover:ml-[-1px] duration-300 bg-[#6fc2b0]'>
+                        <a className='flex justify-between items-center w-full text-gray-300 gap-2' href="/">
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
                     </li>
